91f67b097f20fc54d1143c8b81e4c3825e82f60c
Kevin Molina <kevin.isrrael.molina@gmail.com>
2025-10-25 00:40:04 +0000

    Cambio para imagen aleatoria

--- src/app.js
+++ src/app.js
@@ -6,18 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function () {
-  let who = ['Mi novia', 'Mi abuela', 'Mi tio', 'Mi entrenador'];
-  let action = ['destruyó', 'empeñó', 'vendió', 'escoñetó'];
-  let what = ['mi tarea', 'mis zapatos', 'mi bicicleta'];
-  let when = ['de camino a Tucupita', 'cuando fui al baño', 'cuando estaba estudiando', 'durante mi almuerzo', 'jugando dominó'];
+  let quien = ['Mi novia', 'Mi abuela', 'Mi tio', 'Mi entrenador'];
+  let accion = ['destruyó', 'empeñó', 'vendió', 'escoñetó'];
+  let queHace = ['mi tarea', 'mis zapatos', 'mi bicicleta'];
+  let cuando = ['de camino a Tucupita', 'cuando fui al baño', 'cuando estaba estudiando', 'durante mi almuerzo', 'jugando dominó'];
 
   function generateExcuse() {
-    const whoRandom = who[Math.floor(Math.random() * who.length)];
-    const actionRandom = action[Math.floor(Math.random() * action.length)];
-    const whatRandom = what[Math.floor(Math.random() * what.length)];
-    const whenRandom = when[Math.floor(Math.random() * when.length)];
-    return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+    const quienRandom = quien[Math.floor(Math.random() * quien.length)];
+    const accionRandom = accion[Math.floor(Math.random() * accion.length)];
+    const queHaceRandom = queHace[Math.floor(Math.random() * queHace.length)];
+    const cuandoRandom = cuando[Math.floor(Math.random() * cuando.length)];
+    return `${quienRandom} ${accionRandom} ${queHaceRandom} ${cuandoRandom}.`;
   }
   let p = document.querySelector('#excuse');
   p.innerHTML = generateExcuse();
+
+  const imagenes = [
+    "https://us.123rf.com/450wm/mancunion55/mancunion551610/mancunion55161000008/69509805-horror-mask-on-black-background-with-copyspace-made-from-sugar-and-salt.jpg",
+    "https://wallpapers.com/images/hd/old-skull-on-black-horror-hr8i2zenoeo6mlcj.jpg",
+    "https://i.pinimg.com/564x/7d/34/1c/7d341cac09ed774ee83eacfccbc67821.jpg"
+  ];
+  const indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+  const imagenSeleccionada = imagenes[indiceAleatorio];
+  document.getElementById("imagen-generada").src = imagenSeleccionada;
+
+
+
 };

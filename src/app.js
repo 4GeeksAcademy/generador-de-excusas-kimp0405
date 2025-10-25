2e5c6e978d2656c79d979c671aada75166357ccf
Kevin Molina <kevin.isrrael.molina@gmail.com>
2025-10-25 00:05:49 +0000

    Primer Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  let who = ['Mi novia', 'Mi abuela', 'Mi tio', 'Mi entrenador'];
+  let action = ['destruyó', 'empeñó', 'vendió', 'escoñetó'];
+  let what = ['mi tarea', 'mis zapatos', 'mi bicicleta'];
+  let when = ['de camino a Tucupita', 'cuando fui al baño', 'cuando estaba estudiando', 'durante mi almuerzo', 'jugando dominó'];
+
+  function generateExcuse() {
+    const whoRandom = who[Math.floor(Math.random() * who.length)];
+    const actionRandom = action[Math.floor(Math.random() * action.length)];
+    const whatRandom = what[Math.floor(Math.random() * what.length)];
+    const whenRandom = when[Math.floor(Math.random() * when.length)];
+    return `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}.`;
+  }
+  let p = document.querySelector('#excuse');
+  p.innerHTML = generateExcuse();
 };

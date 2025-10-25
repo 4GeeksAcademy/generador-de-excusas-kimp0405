11bcbb74a49a119357f0c09a35bace2987ca317b
Kevin Molina <kevin.isrrael.molina@gmail.com>
2025-10-25 01:48:50 +0000

    Reducción de texto

--- src/app.js
+++ src/app.js
@@ -5,21 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-  let quien = ['Mi novia', 'Mi abuela', 'Mi tio', 'Mi entrenador'];
-  let accion = ['destruyó', 'empeñó', 'vendió', 'escoñetó'];
-  let queHace = ['mi tarea', 'mis zapatos', 'mi bicicleta'];
-  let cuando = ['de camino a Tucupita', 'cuando fui al baño', 'cuando estaba estudiando', 'durante mi almuerzo', 'jugando dominó'];
+window.onload= () => {
+  const partes = {
+    quien: ['Mi novia', 'Mi abuela', 'Mi tío', 'Mi entrenador'],
+    accion: ['destruyó', 'empeñó', 'vendió', 'escoñetó'],
+    queHace: ['mi tarea', 'mis zapatos', 'mi bicicleta'],
+    cuando: ['de camino a Tucupita', 'cuando fui al baño', 'cuando estaba estudiando', 'durante mi almuerzo', 'jugando dominó']
+  };
 
-  function generateExcuse() {
-    const quienRandom = quien[Math.floor(Math.random() * quien.length)];
-    const accionRandom = accion[Math.floor(Math.random() * accion.length)];
-    const queHaceRandom = queHace[Math.floor(Math.random() * queHace.length)];
-    const cuandoRandom = cuando[Math.floor(Math.random() * cuando.length)];
-    return `${quienRandom} ${accionRandom} ${queHaceRandom} ${cuandoRandom}.`;
-  }
-  let p = document.querySelector('#excuse');
-  p.innerHTML = generateExcuse();
+  const excuse = arr => arr[Math.floor(Math.random() * arr.length)];
+  document.querySelector('#excuse').textContent =
+    `${excuse(partes.quien)} ${excuse(partes.accion)} ${excuse(partes.queHace)} ${excuse(partes.cuando)}.`;
 
   const imagenes = [
     "https://us.123rf.com/450wm/mancunion55/mancunion551610/mancunion55161000008/69509805-horror-mask-on-black-background-with-copyspace-made-from-sugar-and-salt.jpg",
